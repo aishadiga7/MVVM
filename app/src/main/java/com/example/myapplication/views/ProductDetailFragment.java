cf304d113e48a0e5802e2ebeb3f2aea1f3811339
aishwarya <aishwarya.a@techjini.com>
2019-09-25 21:35:23 +0530

    productdetail

---
+++ app/src/main/java/com/example/myapplication/views/ProductDetailFragment.java
@@ -0,0 +1,32 @@
+package com.example.myapplication.views;
+
+import android.database.DatabaseUtils;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import com.example.myapplication.R;
+import com.example.myapplication.databinding.ProductDetailsBinding;
+
+public class ProductDetailFragment extends Fragment {
+
+    private ProductDetailsBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.product_details, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+}
