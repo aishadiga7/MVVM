80bd33bd491987630d489aeb3ddcc690b9cd6d88
aishwarya <aishwarya.a@techjini.com>
2019-09-26 00:11:51 +0530

    added entity

--- app/src/main/java/com/example/myapplication/model/User.java
+++ app/src/main/java/com/example/myapplication/model/User.java
@@ -8,7 +8,7 @@ public class User  {
 
     @PrimaryKey(autoGenerate = true)
     public int id;
-    
+
     @ColumnInfo(name = "first_name")
     public String firstName;
     @ColumnInfo(name = "last_name")
