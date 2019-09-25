3f60ce6986f96b7145349d63e9bb9e0d10c8e988
aishwarya <aishwarya.a@techjini.com>
2019-09-26 00:03:44 +0530

    added room database class

---
+++ app/src/main/java/com/example/myapplication/data/local/AppDataBase.java
@@ -0,0 +1,7 @@
+package com.example.myapplication.data.local;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+@Database(version = 1,entities = {})
+public abstract class AppDataBase extends RoomDatabase {
+}
