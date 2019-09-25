5a380414a9200d1098ca9b8167d8542f3badd806
aishwarya <aishwarya.a@techjini.com>
2019-09-26 00:11:25 +0530

    added entity

--- app/src/main/java/com/example/myapplication/data/local/AppDataBase.java
+++ app/src/main/java/com/example/myapplication/data/local/AppDataBase.java
@@ -2,6 +2,9 @@ package com.example.myapplication.data.local;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
-@Database(version = 1,entities = {})
+
+import com.example.myapplication.model.User;
+
+@Database(version = 1,entities = {User.class})
 public abstract class AppDataBase extends RoomDatabase {
 }
--- app/src/main/java/com/example/myapplication/model/User.java
+++ app/src/main/java/com/example/myapplication/model/User.java
@@ -1,9 +1,17 @@
 package com.example.myapplication.model;
 
-import com.example.myapplication.views.uimodel.Result;
-
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+@Entity
 public class User  {
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+    
+    @ColumnInfo(name = "first_name")
     public String firstName;
+    @ColumnInfo(name = "last_name")
     public String lastName;
 
 }
